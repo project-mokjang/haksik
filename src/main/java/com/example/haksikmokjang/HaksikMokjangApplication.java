72787db1645961eac531c4040b1f7522ff620fe4
dunong <isaacy03@gmail.com>
2026-05-27 21:06:29 +0900

    íproject start

---
+++ src/main/java/com/example/haksikmokjang/HaksikMokjangApplication.java
@@ -0,0 +1,13 @@
+package com.example.haksikmokjang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaksikMokjangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaksikMokjangApplication.class, args);
+    }
+
+}
