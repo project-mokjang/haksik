3a25ebb130486c09f8d992438729f6a08f9859cd
dunong <isaacy03@gmail.com>
2026-06-05 21:11:30 +0900

    develop 충ë돌 해결

--- src/main/java/com/example/haksikmokjang/controller/view/ViewController.java
+++ src/main/java/com/example/haksikmokjang/controller/view/ViewController.java
@@ -31,10 +31,6 @@ public class ViewController {
     public String findPwPage() {
         return "members/find-pw";
     }
-    @GetMapping("/admin")
-    public String adminPage(){
-        return "members/admin-users";
-    }
 
 }
 
