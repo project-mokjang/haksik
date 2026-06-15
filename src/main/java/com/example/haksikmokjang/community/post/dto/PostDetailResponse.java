9688371ea53e3e937a936192dd3049a81c117669
jinwoo <chlwlsdn1215@naver.com>
2026-06-15 20:49:25 +0900

    comment view

--- src/main/java/com/example/haksikmokjang/community/post/dto/PostDetailResponse.java
+++ src/main/java/com/example/haksikmokjang/community/post/dto/PostDetailResponse.java
@@ -1,4 +1,5 @@
 package com.example.haksikmokjang.community.post.dto;
+import com.example.haksikmokjang.community.comment.dto.CommentResponse;
 import lombok.Getter;
 import lombok.Setter;
 import java.time.LocalDateTime;
@@ -15,4 +16,9 @@ public class PostDetailResponse {
     private int viewCount;
     private LocalDateTime createdAt;
     private List<String> imageUrls; //사진들을 화면에 띄울 API 주소
+
+    // 🚨 새롭게 추가될 2가지 관절
+    private boolean isMine; // 이 게시글 자체가 내 글인가?
+    private java.util.List<CommentResponse> comments; // 댓글 목록 통째로 탑재
+
 }
